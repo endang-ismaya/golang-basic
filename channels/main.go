adeb96be4d0c8c38003578bf2d02ee6db67886f8
Endang Ismaya <endang.ismaya@gmail.com>
2022-09-09 20:54:24 +0700

    add functions, channels, interfaces

---
+++ channels/main.go
@@ -0,0 +1,40 @@
+package main
+
+// channels works for communication in GO routine
+
+import (
+	"fmt"
+
+	"github.com/eiannone/keyboard"
+)
+
+var keyPressChan chan rune
+
+func main() {
+	keyPressChan = make(chan rune)
+
+	go listenForKeyPress()
+
+	fmt.Println("Press any key, or q to quit")
+	_ = keyboard.Open()
+
+	defer func() {
+		keyboard.Close()
+	}()
+
+	for {
+		char, _, _ := keyboard.GetSingleKey()
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		keyPressChan <- char
+	}
+}
+
+func listenForKeyPress() {
+	for {
+		key := <-keyPressChan
+		fmt.Println("You pressed", string(key))
+	}
+}
---
+++ functions/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+type Animal struct {
+	Name         string
+	Sound        string
+	NumberOfLegs int
+}
+
+// receiver
+func (a *Animal) Says() {
+	fmt.Printf("A %s says %s\n", a.Name, a.Sound)
+}
+
+func (a *Animal) HowManyLegs() {
+	fmt.Printf("A %s has %d legs\n", a.Name, a.NumberOfLegs)
+}
+
+func main() {
+	z := addTwoNumbers(2, 4)
+	fmt.Println(z)
+
+	x := addTwoNumberSum(5, 6)
+	fmt.Println(x)
+
+	myTotal := sumMany(2, 3, 4, 5)
+	fmt.Println(myTotal)
+
+	// using Animal
+	dog := Animal{
+		Name:         "dog",
+		Sound:        "woof",
+		NumberOfLegs: 4,
+	}
+	dog.Says()
+
+	var cat Animal
+	cat.Name = "cat"
+	cat.Sound = "meow"
+	cat.NumberOfLegs = 4
+
+	cat.Says()
+	cat.HowManyLegs()
+}
+
+func addTwoNumbers(x, y int) int {
+	return x + y
+}
+
+func addTwoNumberSum(x, y int) (sum int) {
+	sum = x + y
+	return
+}
+
+func sumMany(nums ...int) int {
+	total := 0
+
+	for _, i := range nums {
+		total += i
+	}
+
+	return total
+}
