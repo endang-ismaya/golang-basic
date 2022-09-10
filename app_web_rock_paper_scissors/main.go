575d60d83d5d8acfad267878e17c414f488a8565
Endang Ismaya <endang.ismaya@gmail.com>
2022-09-11 00:04:32 +0700

    add app_web_rock_paper_scissors part-1

---
+++ app_web_rock_paper_scissors/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+
+	"endangismaya.com/gotest/app_web_rock_paper_scissors/rps"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html")
+}
+
+func playRound(w http.ResponseWriter, r *http.Request) {
+	winner, computerChoice, roundResult := rps.PlayRound(1)
+	log.Println(winner, computerChoice, roundResult)
+}
+
+func main() {
+	http.HandleFunc("/play", playRound)
+	http.HandleFunc("/", homePage)
+
+	log.Println("Starting web server on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string) {
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
